7dc214a58a905b01ca66a071b39266e188b76610
Majid Mohammadi <majid.mohammadi690@gmail.com>
2025-01-18 22:25:16 +0100

    fs

--- fs_real.py
+++ fs_real.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
         mode = "classification" if type_of_target(y) in ["binary", "multiclass"] else "regression"
         if mode == "classification": 
             y = LabelEncoder().fit_transform(y) 
-        else: continue
+        #else: continue
 
         # Split the dataset into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
